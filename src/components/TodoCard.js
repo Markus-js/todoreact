c281dcc17a6d512994e2caed07b878eb59e76f5c
Markus-js <61780685+Markus-js@users.noreply.github.com>
2022-07-03 14:51:35 +0200

    before typescript

--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -1,49 +1,46 @@
-import React from 'react'
+import React from 'react';
 import { BiUndo } from 'react-icons/bi';
-import { RiDeleteBinLine } from "react-icons/ri";
-import { MdOutlineDone } from "react-icons/md";
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { MdOutlineDone } from 'react-icons/md';
 
-
-export const TodoCard = ({ todo, todos, setTodos }) => {
-
+const TodoCard = ({ todo, todos, setTodos }) => {
   const handleDelete = () => {
-    const newList = todos.filter(t => t.id !== todo.id)
+    const newList = todos.filter(t => t.id !== todo.id);
     setTodos(newList);
     // LOCAL STOTAGE
     window.localStorage.setItem('todo', JSON.stringify(newList));
-  }
+  };
 
   const handleComplete = () => {
-    const newList = todos.filter(t => t.id !== todo.id)
+    const newList = todos.filter(t => t.id !== todo.id);
     todo.completed = !todo.completed;
     setTodos([...newList, todo]);
     // LOCAL STOTAGE
     window.localStorage.setItem('todo', JSON.stringify([...newList, todo]));
-
-  }
-
+  };
 
   return (
-    <article className={todo.completed ? "todo-list__card todo-list__card--completed todo todo--completed" : "todo-list__card todo"} >
-      <h3 className="todo-list__title" >{todo.title}</h3>
+    <article className={todo.completed ? 'todo-list__card todo-list__card--completed todo todo--completed' : 'todo-list__card todo'}>
+      <h3 className="todo-list__title">{todo.title}</h3>
       <div>
         <button
           onClick={handleComplete}
-          className={todo.completed ? "todo--toggle-completed btn btn--yellow" : "todo--toggle-completed btn btn--green"}
-          type="button"
-        >
+          className={todo.completed ? 'todo--toggle-completed btn btn--yellow' : 'todo--toggle-completed btn btn--green'}
+          type="button">
           {todo.completed ? <BiUndo className="icon" /> : <MdOutlineDone className="icon" />}
         </button>
-        {todo.completed &&
-          <button
-            onClick={handleDelete}
-            className="todo__button--remove btn btn--red"
-            type="button"
-          >
-            <RiDeleteBinLine className="icon" />
-          </button>
-        }
+        {todo.completed
+          && (
+            <button
+              onClick={handleDelete}
+              className="todo__button--remove btn btn--red"
+              type="button">
+              <RiDeleteBinLine className="icon" />
+            </button>
+          )}
       </div>
     </article>
-  )
-}
+  );
+};
+
+export default TodoCard;;
